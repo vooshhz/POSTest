2acdff446952f02e353bc6f19b6eb6c35aca9fe8
vooshhz <aadharpatil93@gmail.com>
2025-08-09 13:38:35 -0400

    Initial commit with .gitignore

--- dist-electron/preload.mjs
+++
@@ -1,5 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("api", {
-  addToInventory: (sku, name, qty) => electron.ipcRenderer.invoke("inventory:add", { sku, name, qty })
-});
